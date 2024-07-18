ff8c7dd922742c940c8983aaa73ee015b7aea315
Giwa Mubarak <giwaomogiwa@gmail.com>
2024-07-18 13:36:11 +0100

    Working tree

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./src/**/*.js",    
     "./src/input.css" ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '1rem',
+          md: '3rem',
+          lg: '1rem',
+          xl: '6rem',
+          '2xl': '8rem',
+        },
+      },
+      colors: {
+        'darkpurple': '#120F19',
+        'greycolor': '#B6B3C5',
+        'babyblue': '#7EC8E3',
+        'secondaryColor': '#91E0B4',
+        
+      }
+    },
   },
   plugins: [],
 }
